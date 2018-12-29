4fde108901c9322af6dc0b557ec288018da55e8b
shaburovgena <shaburovgena@gmail.com>
2018-12-29 14:30:19 +0300

    WebSpring 29.12

---
+++ WebSpring/src/main/java/hello/Message.java
@@ -0,0 +1,48 @@
+package hello;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String message;
+
+    public Message() {
+    }
+
+    public Message(String message, String tag) {
+        this.message = message;
+        this.tag = tag;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    private String tag;
+}
